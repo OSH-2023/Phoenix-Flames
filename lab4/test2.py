6ad1636ada7dff439645733a011145b55f41b218
wzc <95694243+Starrybook@users.noreply.github.com>
2023-06-18 20:32:01 +0800

    Update test2.py

--- lab4/test2.py
+++ lab4/test2.py
@@ -5,15 +5,14 @@ import time
 ray.init()
 
 
-@ray.remote
 def matrix_mul(matrix1, matrix2, size):
-    matrix_res = [[0]*size for i in range(size)]
-    for i in range(0, size):
-        for j in range(0, size):
-            matrix_res = 0
+    matrix_res = [[0.0]*size for item in range(size)]
+    for row in range(0, size):
+        for col in range(0, size):
+            matrix_res[row][col] = 0.0
             for k1 in range(0, size):
                 for k2 in range(0, size):
-                    matrix_res += matrix1[i][k1] * matrix2[k2][j]
+                    matrix_res[row][col] += matrix1[row][k1] * matrix2[k2][col]
     return matrix_res
 
 
@@ -22,8 +21,8 @@ class Worker(object):
     def __init__(self):
         self.size = 10
         self.matrix0 = self.matrix_init()
-        self.res = [[0.0]*self.size for item in range(self.size)]
-        self.all_times = 1000000
+        self.res = self.matrix0.copy()
+        self.all_times = 10000
 
     def matrix_init(self):
         matrix = [[0.0]*self.size for item in range(self.size)]
@@ -38,7 +37,7 @@ class Worker(object):
     def calculate(self, times):
         print("I start doing my work.")
         cur_time = time.time()
-        for i in range(times):
+        for k in range(times):
             self.res = matrix_mul(self.res, self.matrix0, self.size)
         print("I have finished my work, duration: ", time.time() - cur_time)
         return self.res
@@ -47,14 +46,14 @@ class Worker(object):
 if __name__ == '__main__':
     worker = Worker.remote()
     worker.all_times = 1000
-    pc_num = 100
+    pc_num = 10
     temps=[]
     for i in range(pc_num):
         temp = worker.calculate.remote(worker.all_times // pc_num)
         temps.append(temp)
 
     result_list = ray.get(temps)
-    result = [[0.0]*worker.size for i in range(worker.size)]
+    result = worker.matrix0
     for m in result_list:
         result = matrix_mul(result, m, worker.size)
     print("final matrix: \n", result)
