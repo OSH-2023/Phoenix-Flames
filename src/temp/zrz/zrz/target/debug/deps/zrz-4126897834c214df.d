47f9558f9fdb6463513bc7e65e4f233d5c410fd9
Des_W <Des_W@outlook.com>
2023-07-04 16:25:28 +0800

    add how-to-build-from-kernel_all

---
+++ src/temp/zrz/zrz/target/debug/deps/zrz-4126897834c214df.d
@@ -0,0 +1,6 @@
+/home/desw/OSH_Project/Phoenix-Flames/src/temp/zrz/zrz/target/debug/deps/zrz-4126897834c214df.rmeta: src/main.rs src/seL4_data.rs
+
+/home/desw/OSH_Project/Phoenix-Flames/src/temp/zrz/zrz/target/debug/deps/zrz-4126897834c214df.d: src/main.rs src/seL4_data.rs
+
+src/main.rs:
+src/seL4_data.rs:
---
+++ src/temp/zrz/zrz/target/debug/deps/zrz-a8f76b1b0c425dd8.d
@@ -0,0 +1,6 @@
+/home/desw/OSH_Project/Phoenix-Flames/src/temp/zrz/zrz/target/debug/deps/zrz-a8f76b1b0c425dd8.rmeta: src/main.rs src/seL4_data.rs
+
+/home/desw/OSH_Project/Phoenix-Flames/src/temp/zrz/zrz/target/debug/deps/zrz-a8f76b1b0c425dd8.d: src/main.rs src/seL4_data.rs
+
+src/main.rs:
+src/seL4_data.rs:
