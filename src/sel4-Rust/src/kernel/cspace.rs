48e96fbeb03291e18f53440c143e4d711cb4524e
jwh030929 <118949207+PB21151767@users.noreply.github.com>
2023-07-05 09:45:53 +0800

    Update cspace.rs

--- src/sel4-Rust/src/kernel/cspace.rs
+++ src/sel4-Rust/src/kernel/cspace.rs
@@ -47,187 +47,174 @@ pub struct resolveAddressBits_ret<'a> {
 }
 pub type resolveAddressBits_ret_t<'a> = resolveAddressBits_ret<'a>;
 
-
-//  pub fn lookupCap(thread:&tcb_t,  cPtr:cptr_t)->lookupCap_ret_t
-// {
-//     let lu_ret:lookupSlot_raw_ret_t;
-//     let ret:lookupCap_ret_t;
-
-//     lu_ret = lookupSlot(thread, cPtr);
-//     if (unlikely(lu_ret.status != EXCEPTION_NONE)) {
-//         ret.status = lu_ret.status;
-//         ret.cap = cap_null_cap_new();
-//         return ret;
-//     }
-
-//     ret.status = EXCEPTION_NONE;
-//     ret.cap = lu_ret.slot->cap;
-//     return ret;
-// }
+pub fn lookupCap(thread: *mut tcb_t, cPtr: u64) -> lookupCap_ret_t {
+    let mut lu_ret = lookupSlot(thread, cPtr);
+    if lu_ret.status != 0u64 {
+        return lookupCap_ret_t {
+            status: lu_ret.status,
+            cap: cap_null_cap_new(),
+        };
+    }
+    lookupCap_ret_t {
+        status: 0u64,
+        cap: (*lu_ret.slot).cap,
+    }
+}
 
 
-// lookupCapAndSlot_ret_t lookupCapAndSlot(tcb_t *thread, cptr_t cPtr)
-// {
-//     lookupSlot_raw_ret_t lu_ret;
-//     lookupCapAndSlot_ret_t ret;
+pub fn lookupCapAndSlot(thread: *mut  tcb_t,cPtr:u64)->lookupCapAndSlot_ret_t
+{
+    let mut lu_ret=lookupSlot(thread,cPtr);
+    if lu_ret.status != 0u64 {
+        return lookupCapAndSlot_ret_t{
+            status:lu_ret.status,
+            slot:std::ptr::null_mut(),
+            cap:cap_null_cap_new(),
+        }
+    }
+    lookupCapAndSlot_ret_t {
+        status: 0u64,
+        cap: (*lu_ret.slot).cap,
+        slot: lu_ret.slot,
+    }
+}
 
-//     lu_ret = lookupSlot(thread, cPtr);
-//     if (unlikely(lu_ret.status != EXCEPTION_NONE)) {
-//         ret.status = lu_ret.status;
-//         ret.slot = NULL;
-//         ret.cap = cap_null_cap_new();
-//         return ret;
-//     }
 
-//     ret.status = EXCEPTION_NONE;
-//     ret.slot = lu_ret.slot;
-//     ret.cap = lu_ret.slot->cap;
-//     return ret;
-// }
+pub fn lookupSlot(thread: *mut tcb_t, capptr:u64 )->lookupSlot_raw_ret_t
+{
+    let threadRoot = (*tcb_ptr_cte_ptr(thread, tcb_cnode_index::tcbCTable as u64)).cap;
+    let res_ret = resolveAddressBits(threadRoot, capptr, wordBits);
+    lookupSlot_raw_ret_t {
+        status: res_ret.status,
+        slot: res_ret.slot,
+    }
+}
 
-// pub fn lookupSlot(thread:&tcb_t, capptr:cptr_t )->lookupSlot_raw_ret_t
-// {
-//     let threadRoot:cap_t ;
-//     let res_ret:resolveAddressBits_ret_t;
-//     let ret:lookupSlot_raw_ret_t;
+pub fn lookupSlotForCNodeOp(isSource:bool_t,root:cap_t,capptr:u64,depth:u64)->lookupSlot_ret_t
+{
+    let res_ret:resolveAddressBits_ret_t ;
+    let ret:lookupSlot_ret_t;
+    
+    ret.slot=std::ptr::null_mut();
+    if cap_get_capType(root) != cap_tag_t::cap_cnode_cap as u64 {
+        current_syscall_error.type_ = seL4_Error::seL4_FailedLookup as u64;
+        current_syscall_error.failedLookupWasSource = isSource;
+        current_lookup_fault = lookup_fault_invalid_root_new();
+        return lookupSlot_ret_t {
+            status: exception::EXCEPTION_SYSCALL_ERROR as u64,
+            slot: std::ptr::null_mut()
+        };
+    }
+    if depth < 1 || depth > wordBits {
+        current_syscall_error.type_ = seL4_Error::seL4_RangeError as u64;
+        current_syscall_error.rangeErrorMin = 1;
+        current_syscall_error.rangeErrorMax = wordBits;
+        return lookupSlot_ret_t {
+            status: exception::EXCEPTION_SYSCALL_ERROR as u64,
+            slot: std::ptr::null_mut(),
+        };
+    }
 
-//     threadRoot = TCB_PTR_CTE_PTR(thread, tcbCTable)->cap;
-//     res_ret = resolveAddressBits(threadRoot, capptr, wordBits);
+    let res_ret = resolveAddressBits(root, capptr, depth);
+    if res_ret.status != 0u64 {
+        current_syscall_error.type_ = seL4_Error::seL4_FailedLookup as u64;
+        current_syscall_error.failedLookupWasSource = isSource;
+        return lookupSlot_ret_t {
+            status: exception::EXCEPTION_SYSCALL_ERROR as u64,
+            slot: exception::EXCEPTION_SYSCALL_ERROR as u64,
+        };
+    }
+    if res_ret.bitsRemaining != 0 {
+        current_syscall_error.type_ = seL4_Error::seL4_FailedLookup as u64;
+        current_syscall_error.failedLookupWasSource = isSource;
+        current_lookup_fault = lookup_fault_depth_mismatch_new(0, res_ret.bitsRemaining);
+        return lookupSlot_ret_t {
+            status: exception::EXCEPTION_SYSCALL_ERROR as u64,
+            slot: exception::EXCEPTION_SYSCALL_ERROR as u64,
+        };
+    }
+    lookupSlot_ret_t {
+        status: 0u64,
+        slot: res_ret.slot,
+    }
+}
+pub  fn lookupSourceSlot(
+    root: cap_t,
+    capptr: u64,
+    depth: u64,
+) -> lookupSlot_ret_t {
+    lookupSlotForCNodeOp(1u64, root, capptr, depth)
+}
 
-//     ret.status = res_ret.status;
-//     ret.slot = res_ret.slot;
-//     return ret;
-// }
 
-// lookupSlot_ret_t lookupSlotForCNodeOp(bool_t isSource, cap_t root, cptr_t capptr,
-//                                       word_t depth)
-// {
-//     resolveAddressBits_ret_t res_ret;
-//     lookupSlot_ret_t ret;
+pub fn lookupTargetSlot(
+    root: cap_t,
+    capptr: u64,
+    depth: u64,
+) -> lookupSlot_ret_t {
+    lookupSlotForCNodeOp(0u64, root, capptr, depth)
+}
 
-//     ret.slot = NULL;
+pub fn lookupPivotSlot(root: cap_t, capptr: u64, depth: u64) -> lookupSlot_ret_t {
+    lookupSlotForCNodeOp(1u64, root, capptr, depth)
+}
 
-//     if (unlikely(cap_get_capType(root) != cap_cnode_cap)) {
-//         current_syscall_error.type = seL4_FailedLookup;
-//         current_syscall_error.failedLookupWasSource = isSource;
-//         current_lookup_fault = lookup_fault_invalid_root_new();
-//         ret.status = EXCEPTION_SYSCALL_ERROR;
-//         return ret;
-//     }
+macro_rules! MASK {
+    ($x:expr) => {
+        (1u64 << ($x)) - 1u64
+    };
+}
 
-//     if (unlikely(depth < 1 || depth > wordBits)) {
-//         current_syscall_error.type = seL4_RangeError;
-//         current_syscall_error.rangeErrorMin = 1;
-//         current_syscall_error.rangeErrorMax = wordBits;
-//         ret.status = EXCEPTION_SYSCALL_ERROR;
-//         return ret;
-//     }
-//     res_ret = resolveAddressBits(root, capptr, depth);
-//     if (unlikely(res_ret.status != EXCEPTION_NONE)) {
-//         current_syscall_error.type = seL4_FailedLookup;
-//         current_syscall_error.failedLookupWasSource = isSource;
-//         /* current_lookup_fault will have been set by resolveAddressBits */
-//         ret.status = EXCEPTION_SYSCALL_ERROR;
-//         return ret;
-//     }
 
-//     if (unlikely(res_ret.bitsRemaining != 0)) {
-//         current_syscall_error.type = seL4_FailedLookup;
-//         current_syscall_error.failedLookupWasSource = isSource;
-//         current_lookup_fault =
-//             lookup_fault_depth_mismatch_new(0, res_ret.bitsRemaining);
-//         ret.status = EXCEPTION_SYSCALL_ERROR;
-//         return ret;
-//     }
+pub fn resolveAddressBits(
+    mut nodeCap: cap_t,
+    capptr: u64,
+    mut n_bits: u64,
+) -> resolveAddressBits_ret_t {
+    let mut ret = resolveAddressBits_ret_t {
+        status: 0u64,
+        slot:std::ptr::null_mut(),
+        bitsRemaining: n_bits,
+    };
+    if cap_get_capType(nodeCap) != cap_tag_t::cap_cnode_cap as u64 {
+        current_lookup_fault = lookup_fault_invalid_root_new();
+        ret.status = exception::EXCEPTION_LOOKUP_FAULT as u64;
+        return ret;
+    }
 
-//     ret.slot = res_ret.slot;
-//     ret.status = EXCEPTION_NONE;
-//     return ret;
-// }
-
-// lookupSlot_ret_t lookupSourceSlot(cap_t root, cptr_t capptr, word_t depth)
-// {
-//     return lookupSlotForCNodeOp(true, root, capptr, depth);
-// }
-
-// lookupSlot_ret_t lookupTargetSlot(cap_t root, cptr_t capptr, word_t depth)
-// {
-//     return lookupSlotForCNodeOp(false, root, capptr, depth);
-// }
-
-// lookupSlot_ret_t lookupPivotSlot(cap_t root, cptr_t capptr, word_t depth)
-// {
-//     return lookupSlotForCNodeOp(true, root, capptr, depth);
-// }
-
-// resolveAddressBits_ret_t resolveAddressBits(cap_t nodeCap, cptr_t capptr, word_t n_bits)
-// {
-//     resolveAddressBits_ret_t ret;
-//     word_t radixBits, guardBits, levelBits, guard;
-//     word_t capGuard, offset;
-//     cte_t *slot;
-
-//     ret.bitsRemaining = n_bits;
-//     ret.slot = NULL;
-
-//     if (unlikely(cap_get_capType(nodeCap) != cap_cnode_cap)) {
-//         current_lookup_fault = lookup_fault_invalid_root_new();
-//         ret.status = EXCEPTION_LOOKUP_FAULT;
-//         return ret;
-//     }
-
-//     while (1) {
-//         radixBits = cap_cnode_cap_get_capCNodeRadix(nodeCap);
-//         guardBits = cap_cnode_cap_get_capCNodeGuardSize(nodeCap);
-//         levelBits = radixBits + guardBits;
-
-//         /* Haskell error: "All CNodes must resolve bits" */
-//         assert(levelBits != 0);
-
-//         capGuard = cap_cnode_cap_get_capCNodeGuard(nodeCap);
-
-//         /* The MASK(wordRadix) here is to avoid the case where
-//          * n_bits = wordBits (=2^wordRadix) and guardBits = 0, as it violates
-//          * the C spec to shift right by more than wordBits-1.
-//          */
-//         guard = (capptr >> ((n_bits - guardBits) & MASK(wordRadix))) & MASK(guardBits);
-//         if (unlikely(guardBits > n_bits || guard != capGuard)) {
-//             current_lookup_fault =
-//                 lookup_fault_guard_mismatch_new(capGuard, n_bits, guardBits);
-//             ret.status = EXCEPTION_LOOKUP_FAULT;
-//             return ret;
-//         }
-
-//         if (unlikely(levelBits > n_bits)) {
-//             current_lookup_fault =
-//                 lookup_fault_depth_mismatch_new(levelBits, n_bits);
-//             ret.status = EXCEPTION_LOOKUP_FAULT;
-//             return ret;
-//         }
-
-//         offset = (capptr >> (n_bits - levelBits)) & MASK(radixBits);
-//         slot = CTE_PTR(cap_cnode_cap_get_capCNodePtr(nodeCap)) + offset;
-
-//         if (likely(n_bits == levelBits)) {
-//             ret.status = EXCEPTION_NONE;
-//             ret.slot = slot;
-//             ret.bitsRemaining = 0;
-//             return ret;
-//         }
-
-//         /** GHOSTUPD: "(\<acute>levelBits > 0, id)" */
-
-//         n_bits -= levelBits;
-//         nodeCap = slot->cap;
-
-//         if (unlikely(cap_get_capType(nodeCap) != cap_cnode_cap)) {
-//             ret.status = EXCEPTION_NONE;
-//             ret.slot = slot;
-//             ret.bitsRemaining = n_bits;
-//             return ret;
-//         }
-//     }
-
-//     UNREACHABLE();
-// }
-
+    loop {
+        let radixBits = cap_cnode_cap_get_capCNodeRadix(nodeCap);
+        let guardBits = cap_cnode_cap_get_capCNodeGuardSize(nodeCap);
+        let levelBits = radixBits + guardBits;
+        let capGuard = cap_cnode_cap_get_capCNodeGuard(nodeCap);
+        let guard: u64 = (capptr >> ((n_bits - guardBits) & MASK!(wordRadix))) & MASK!(guardBits);
+        if guardBits > n_bits || guard != capGuard {
+            current_lookup_fault = lookup_fault_guard_mismatch_new(capGuard, n_bits, guardBits);
+            ret.status = exception::EXCEPTION_LOOKUP_FAULT as u64;
+            return ret;
+        }
+        if levelBits > n_bits {
+            current_lookup_fault = lookup_fault_depth_mismatch_new(levelBits, n_bits);
+            ret.status = exception::EXCEPTION_LOOKUP_FAULT as u64;
+            return ret;
+        }
+        let offset: u64 = (capptr >> (n_bits - levelBits)) & MASK!(radixBits);
+        let slot = CTE_PTR(cap_cnode_cap_get_capCNodePtr(nodeCap)) + offset;
+        if n_bits <= levelBits {
+            ret.status = 0u64;
+            ret.slot = slot;
+            ret.bitsRemaining = 0;
+            return ret;
+        }
+        n_bits -= levelBits;
+        nodeCap = (*slot).cap;
+        if cap_get_capType(nodeCap) != cap_tag_t::cap_cnode_cap as u64 {
+            ret.status = exception::EXCEPTION_NONE as u64;
+            ret.slot = slot;
+            ret.bitsRemaining = n_bits;
+            return ret;
+        }
+    }
+    ret.status = 0u64;
+    ret
+}
