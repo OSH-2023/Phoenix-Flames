7609cf1a162bc546b147bb2a59a6f7def4a9a66e
Des_W <Des_W@outlook.com>
2023-07-07 15:25:51 +0800

    minor changes

--- src/sel4-Rust/sel4-Rust/target/debug/deps/lazy_static-4d6e65ac8e1d305a.d
+++
@@ -1,6 +0,0 @@
-C:\My Files\OSH-project\Phoenix-Flames\src\sel4-Rust\sel4-Rust\target\debug\deps\lazy_static-4d6e65ac8e1d305a.rmeta: C:\Users\26524\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\lib.rs C:\Users\26524\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\core_lazy.rs
-
-C:\My Files\OSH-project\Phoenix-Flames\src\sel4-Rust\sel4-Rust\target\debug\deps\lazy_static-4d6e65ac8e1d305a.d: C:\Users\26524\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\lib.rs C:\Users\26524\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\core_lazy.rs
-
-C:\Users\26524\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\lib.rs:
-C:\Users\26524\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\core_lazy.rs:
--- src/sel4-Rust/target/riscv64gc-unknown-none-elf/debug/deps/spin-bdea706c5905858f.d
+++
@@ -1,8 +0,0 @@
-/home/vinson/Desktop/Phoenix-Flames/src/sel4-Rust/target/riscv64gc-unknown-none-elf/debug/deps/spin-bdea706c5905858f.rmeta: /home/vinson/.cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/lib.rs /home/vinson/.cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/mutex.rs /home/vinson/.cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/rw_lock.rs /home/vinson/.cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/once.rs
-
-/home/vinson/Desktop/Phoenix-Flames/src/sel4-Rust/target/riscv64gc-unknown-none-elf/debug/deps/spin-bdea706c5905858f.d: /home/vinson/.cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/lib.rs /home/vinson/.cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/mutex.rs /home/vinson/.cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/rw_lock.rs /home/vinson/.cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/once.rs
-
-/home/vinson/.cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/lib.rs:
-/home/vinson/.cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/mutex.rs:
-/home/vinson/.cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/rw_lock.rs:
-/home/vinson/.cargo/registry/src/github.com-1ecc6299db9ec823/spin-0.5.2/src/once.rs:
