59a3f6416df67e8a7ed6aa74e4def304971b8cce
Starrybook <1647849656@qq.com>
2023-07-08 16:24:56 +0800

    7/8

---
+++ src/sel4-Rust/sel4-Rust/target/debug/deps/lazy_static-2cce767740a67024.d
@@ -0,0 +1,6 @@
+/home/tern/Desktop/work-space/copy2/copy3/Phoenix-Flames/src/sel4-Rust/sel4-Rust/target/debug/deps/lazy_static-2cce767740a67024.rmeta: /home/tern/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/lazy_static-1.4.0/src/lib.rs /home/tern/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/lazy_static-1.4.0/src/core_lazy.rs
+
+/home/tern/Desktop/work-space/copy2/copy3/Phoenix-Flames/src/sel4-Rust/sel4-Rust/target/debug/deps/lazy_static-2cce767740a67024.d: /home/tern/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/lazy_static-1.4.0/src/lib.rs /home/tern/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/lazy_static-1.4.0/src/core_lazy.rs
+
+/home/tern/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/lazy_static-1.4.0/src/lib.rs:
+/home/tern/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/lazy_static-1.4.0/src/core_lazy.rs:
---
+++ src/sel4-Rust/sel4-Rust/target/debug/deps/sel4_rust-cdae669740c5a2c0.d
@@ -0,0 +1,34 @@
+/home/tern/Desktop/work-space/copy2/copy3/Phoenix-Flames/src/sel4-Rust/sel4-Rust/target/debug/deps/sel4_rust-cdae669740c5a2c0.rmeta: src/lib.rs src/lang_items.rs src/kernel/mod.rs src/kernel/cspace.rs src/kernel/thread.rs src/kernel/notification.rs src/kernel/fastpath.rs src/kernel/tcb.rs src/types/mod.rs src/types/basic_types.rs src/types/compound_types.rs src/types/stdint.rs src/libsel4/mod.rs src/libsel4/shared_types.rs src/libsel4/constants.rs src/libsel4/errors.rs src/libsel4/objecttype.rs src/object.rs src/failures.rs src/model/mod.rs src/model/statedata.rs src/machine/mod.rs src/machine/hardware.rs src/machine/registerset.rs src/util.rs src/inlines.rs src/cnode.rs Cargo.toml
+
+/home/tern/Desktop/work-space/copy2/copy3/Phoenix-Flames/src/sel4-Rust/sel4-Rust/target/debug/deps/sel4_rust-cdae669740c5a2c0.d: src/lib.rs src/lang_items.rs src/kernel/mod.rs src/kernel/cspace.rs src/kernel/thread.rs src/kernel/notification.rs src/kernel/fastpath.rs src/kernel/tcb.rs src/types/mod.rs src/types/basic_types.rs src/types/compound_types.rs src/types/stdint.rs src/libsel4/mod.rs src/libsel4/shared_types.rs src/libsel4/constants.rs src/libsel4/errors.rs src/libsel4/objecttype.rs src/object.rs src/failures.rs src/model/mod.rs src/model/statedata.rs src/machine/mod.rs src/machine/hardware.rs src/machine/registerset.rs src/util.rs src/inlines.rs src/cnode.rs Cargo.toml
+
+src/lib.rs:
+src/lang_items.rs:
+src/kernel/mod.rs:
+src/kernel/cspace.rs:
+src/kernel/thread.rs:
+src/kernel/notification.rs:
+src/kernel/fastpath.rs:
+src/kernel/tcb.rs:
+src/types/mod.rs:
+src/types/basic_types.rs:
+src/types/compound_types.rs:
+src/types/stdint.rs:
+src/libsel4/mod.rs:
+src/libsel4/shared_types.rs:
+src/libsel4/constants.rs:
+src/libsel4/errors.rs:
+src/libsel4/objecttype.rs:
+src/object.rs:
+src/failures.rs:
+src/model/mod.rs:
+src/model/statedata.rs:
+src/machine/mod.rs:
+src/machine/hardware.rs:
+src/machine/registerset.rs:
+src/util.rs:
+src/inlines.rs:
+src/cnode.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
